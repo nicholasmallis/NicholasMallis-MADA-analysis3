072b91a0ac99aa0b452aefd6bdb05007f7e15026
nicholasmallis <nam25576@uga.edu>
2021-11-08 14:30:17 -0500

    Correcting LASSO and RF
    using Dr. Handel's ex

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -32,6 +32,51 @@ processeddata <- na.omit(processeddata)
 #checking
 glimpse(processeddata)
 
+
+
+# For those symptoms where you have both multiple levels and yes/no, 
+# remove all the yes/no versions. That should remove 4 variables.
+
+
+processeddata <- select(processeddata, -c(WeaknessYN, CoughYN, CoughYN2, MyalgiaYN))
+
+glimpse(processeddata)
+
+# We also want to code the 3 ordinal/multi-level factors as ordered, 
+# so make sure they are coded as ordered factors. 
+# The order should of course be None/Mild/Moderate/Severe.
+
+#as.factor(data$Weakness)
+#table(data$Weakness)
+
+#as.factor(data$CoughIntensity)
+#table(data$CoughIntensity)
+
+#as.factor(data$Myalgia)
+#table(data$Myalgia)
+
+
+processeddata <- mutate(processeddata, Weakness = factor(Weakness, levels = c("None", "Mild",
+                                                            "Moderate","Severe"),ordered = TRUE))
+processeddata <- mutate(processeddata, CoughIntensity = factor(CoughIntensity, levels = c("None", "Mild",
+                                                                        "Moderate","Severe"),ordered = TRUE))
+processeddata <- mutate(processeddata, Myalgia = factor(Myalgia, levels = c("None", "Mild",
+                                                          "Moderate","Severe"),ordered = TRUE))
+
+# But it’s often better to decide manually for each variable based on your scientific 
+# expertise if you want to remove it or not. We’ll take that approach here.
+# After looking at the data, we decide to remove those binary predictors that 
+# have <50 entries in one category (there are 2). Write code to remove them.
+
+library(table1) #loading the table 1 package
+
+table1 <- table1(~ . , data=processeddata, overall="Total")
+table1
+
+# Looks like hearing and vision both don't have more than 50 yes
+processeddata <- select(processeddata, -c(Hearing, Vision))
+
+
 # location to save file
 save_data_location <- here::here("data","processed_data","processeddata.rds")
 saveRDS(processeddata, file = save_data_location)
